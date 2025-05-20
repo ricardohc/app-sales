9df35691167d642de196e0c633f90613c30e1c66
Ricardo Huacso <rhuacso@gmail.com>
2025-05-20 00:36:48 -0500

    feat: Agregando metodo sumar()

---
+++ src/main/java/pe/com/demo/Application.java
@@ -0,0 +1,18 @@
+package pe.com.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+        OperacionesRicardo oRic=new OperacionesRicardo();
+        oRic.sumar(10, 20);
+    }
+
+
+
+}
+
